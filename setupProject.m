d648268f8f373b9f42a768a1bed5f3c9bed43b03
Jaiden <jaidend1273@gmail.com>
2026-07-16 19:37:00 -0400

    added setup

---
+++ setupProject.m
@@ -0,0 +1,9 @@
+projectRoot = fileparts(mfilename("fullpath"));
+
+addpath(fullfile(projectRoot, "config"));
+addpath(fullfile(projectRoot, "models", "harness"));
+addpath(fullfile(projectRoot, "models", "plants"));
+addpath(fullfile(projectRoot, "src"));
+
+VehicleStateBus = stateBus();
+run(fullfile(projectRoot, "config", "eClassParams.m"));
