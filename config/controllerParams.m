811a0fcf2371016277b47b491e27efaa8c03ca67
Jaiden <jaidend1273@gmail.com>
2026-07-21 11:02:54 -0400

    test harness finished

---
+++ config/controllerParams.m
@@ -0,0 +1,3 @@
+CTRL_BASELINE = 0;
+CTRL_AFS      = 1;
+CTRL_TV       = 2;
--- setupProject.m
+++ setupProject.m
@@ -2,10 +2,18 @@ projectRoot = fileparts(mfilename("fullpath"));
 
 addpath(fullfile(projectRoot, "config", "scenarios"));
 addpath(fullfile(projectRoot, "models", "harness"));
+addpath(fullfile(projectRoot, "models", "controllers"));
 addpath(fullfile(projectRoot, "models", "plants"));
 addpath(fullfile(projectRoot, "src"));
 
 VehicleStateBus = stateBus();
+ReferenceBus = referenceBus();
+
 run(fullfile(projectRoot, "config", "eClassParams.m"));
+run(fullfile(projectRoot, "config", "controllerParams.m"));
 
 scenario = curvedLaneParams();
+
+controllerMode = CTRL_BASELINE;
+% controllerMode = CTRL_AFS;
+% controllerMode = CTRL_TV;
