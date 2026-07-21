e4a6f48e532bca2e46c1be8e457d8edb29fe301c
Jaiden <jaidend1273@gmail.com>
2026-07-21 11:21:08 -0400

    fixed variant bug

--- config/controllerParams.m
+++ config/controllerParams.m
@@ -1,3 +1,12 @@
 CTRL_BASELINE = 0;
 CTRL_AFS      = 1;
 CTRL_TV       = 2;
+
+Choice = Simulink.VariantExpression( ...
+    "controllerMode == CTRL_TV");
+
+Choice_1 = Simulink.VariantExpression( ...
+    "controllerMode == CTRL_AFS");
+
+Choice_2 = Simulink.VariantExpression( ...
+    "controllerMode == CTRL_BASELINE");
